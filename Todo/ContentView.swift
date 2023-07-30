094515d3e4af117ec286e887ca8624bf52412830
Majed <boumaged@gmail.com>
2023-07-30 14:17:42 +0100

    add a navigation stack

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var todos = [
+        Todo( title: "Feed the cat"),
+        Todo( title: "Play with cat"),
+    Todo( title: "Get allergies"),
+    Todo( title: "Run a way from cat"),
+        Todo( title: "Get a new cat")]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                List(todos, id: \.id) { todo in
+                    Text(todo.title)
+                        
+                }
+                .navigationTitle("Todos")
+            }
+            
         }
-        .padding()
     }
 }
 
---
+++ Todo/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  Todo
+//
+//  Created by Majed on 30/07/2023.
+//
+
+import Foundation
+
+struct Todo : Identifiable{
+    let id = UUID()
+    var title : String
+    var isCompleted = false
+}
